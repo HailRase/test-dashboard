7b2e47e54d2132d027eef8d570f1c8477359b790
Юра <yurii_klimko@mail.ru>
2023-04-07 09:19:41 +0300

    test

--- src/s2-features/f2-monitoring-cc/MonitoringCC.tsx
+++ src/s2-features/f2-monitoring-cc/MonitoringCC.tsx
@@ -3,12 +3,27 @@ import s from './MonitoringCC.module.scss'
 import DiagramCallRating from "../f2-monitoring-cc/diagram-call-rating/DiagramCallRating";
 import OperatorRating from "../f2-monitoring-cc/operator-raiting/OperatorRating";
 import Histogram from "../f2-monitoring-cc/histogram/Histogram";
+import {ReactComponent as Home} from "../../assets/home-icon.svg";
+import {useNavigate} from "react-router-dom";
+
+
+
 
 const MonitoringCc = () => {
+
+    const navigate = useNavigate()
+
+    const onHomeHandler = () => {
+        navigate('/')
+    }
+
     return (
         <div className={s.monitoringCCWrapper}>
             <div className={s.monitoringCCContainer}>
-                <div className={s.monitoringCCHeader}><span>Мониторинг Контакт-центра (Past)</span></div>
+                <div className={s.monitoringCCHeader}>
+                    <Home className={s.homeLogo} onClick={onHomeHandler}/>
+                    <span>Мониторинг Контакт-центра (Past)</span>
+                </div>
                 <div className={s.callAndOperatorRating}>
                     <DiagramCallRating/>
                     <OperatorRating/>
--- src/s2-features/f3-call-report/CallReport.tsx
+++ src/s2-features/f3-call-report/CallReport.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import s from './CallReport.module.scss'
 import CallReportTable from "./CallReportTable/CallReportTable";
+import {ReactComponent as Home} from "../../assets/home-icon.svg";
+import {useNavigate} from "react-router-dom";
 
 const CallReport = () => {
+
+    const navigate = useNavigate()
+
+    const onHomeHandler = () => {
+        navigate('/')
+    }
+
     return (
         <div className={s.callReportWrapper}>
             <div className={s.callReportContainer}>
                 <div className={s.callReportHeader}>
+                    <Home className={s.homeLogo} onClick={onHomeHandler}/>
                     <span>Статистика по звонкам</span>
                 </div>
                 <CallReportTable/>
