d66a5d38b0a3fc9f5284eb035fb8245ccae07327
HailRase <yurii_klimko@mail.ru>
2023-06-28 20:47:10 +0300

    set changes

--- src/s1-main/m3-dal/d2-api/monitoringCCPastAPI.ts
+++ src/s1-main/m3-dal/d2-api/monitoringCCPastAPI.ts
@@ -2,15 +2,15 @@ import {instance} from "../d1-instance/instance";
 
 export const monitoringCCPastAPI = {
     getPastHistogramData(dateStart: string, timeStart: string, dateEnd: string, timeEnd: string) {
-        return instance.get('dashboard2_schema')
+        return instance.get(`dashboard2_schema?dtStart=${dateStart}T${timeStart}:59.000&dtStop=${dateEnd}T${timeEnd}:59.000`)
     },
     getPastOuterPieData(dateStart: string, timeStart: string, dateEnd: string, timeEnd: string){
-        return instance.get('dashboard2_day_code')
+        return instance.get(`dashboard2_day_code?dtStart=${dateStart}T${timeStart}:59.000&dtStop=${dateEnd}T${timeEnd}:59.000`)
     },
     getPastInnerPieData(dateStart: string, timeStart: string, dateEnd: string, timeEnd: string){
-        return instance.get('dashboard2_day_all')
+        return instance.get(`dashboard2_day_all?dtStart=${dateStart}T${timeStart}:59.000&dtStop=${dateEnd}T${timeEnd}:59.000`)
     },
     getPastTableData(dateStart: string, timeStart: string, dateEnd: string, timeEnd: string){
-        return instance.get('dashboard2_table')
+        return instance.get(`dashboard2_table?dtStart=${dateStart}T${timeStart}:59.000&dtStop=${dateEnd}T${timeEnd}:59.000`)
     }
 }
--- src/s2-features/f2-monitoring-cc/past/MonitoringCCPast.tsx
+++ src/s2-features/f2-monitoring-cc/past/MonitoringCCPast.tsx
@@ -4,7 +4,6 @@ import Histogram from "../../../common/components/Histogram/Histogram";
 import {useNavigate} from "react-router-dom";
 import {Sidebar} from "../../../common/components/Sidebar/Sidebar";
 import {PATH} from "../../../common/routes/routes";
-import CustomTabs from "../../../common/components/CustomTabs/CustomTabs";
 import ArrowLeftIcon from "../../../common/components/ArrowLeftIcon/ArrowLeftIcon";
 import OptionIcon from "../../../common/components/OptionIcon/OptionIcon";
 import HomeIcon from "../../../common/components/HomeIcon/HomeIcon";
@@ -16,6 +15,12 @@ import useIsAuth from "../../../common/hooks/useIsAuth";
 import {useDispatch} from "react-redux";
 import {useScale} from "../../../common/hooks/useScale";
 import {findMaxAcceptAndNotAcceptSum} from "../../../common/utils/findMaxAcceptAndNotAcceptSum";
+import moment from "moment/moment";
+import Form from "react-bootstrap/Form";
+import TabButton from "../../../common/components/TabButton/TabButton";
+import {fetchPastTableData} from "../../../s1-main/m2-bll/b2-monitoring-past-reducer/pastTable-reducer";
+import {fetchPastHistogramData} from "../../../s1-main/m2-bll/b2-monitoring-past-reducer/pastHistogram-reducer";
+import {fetchPastPieData} from "../../../s1-main/m2-bll/b2-monitoring-past-reducer/pastPie-reducer";
 
 
 const MonitoringCCPast = () => {
@@ -24,12 +29,16 @@ const MonitoringCCPast = () => {
     const [isActive, setIsActive] = useState<boolean>(false)
     const navigate = useNavigate()
     const isAuth = useIsAuth()
+    const [dateStart, setDateStart] = useState(moment().format("YYYY-MM-DD"))
+    const [timeStart, setTimeStart] = useState("00:00")
+    const [dateEnd, setDateEnd] = useState(moment().format("YYYY-MM-DD"))
+    const [timeEnd, setTimeEnd] = useState("23:59")
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch<any>()
 
     useEffect(() => {
         if (!isAuth) navigate('/')
-    },[])
+    }, [])
     const onHomeHandler = () => {
         navigate(`${PATH.HOME}`)
     }
@@ -40,11 +49,30 @@ const MonitoringCCPast = () => {
         setIsActive(false)
     }
 
+    const onDateStartChangeHandler = (e: any) => {
+        setDateStart(e.currentTarget.value)
+    }
+    const onTimeStartChangeHandler = (e: any) => {
+        setTimeStart(e.currentTarget.value)
+    }
+    const onDateEndChangeHandler = (e: any) => {
+        setDateEnd(e.currentTarget.value)
+    }
+    const onTimeEndChangeHandler = (e: any) => {
+        setTimeEnd(e.currentTarget.value)
+    }
+
+    const onLoadDataHandler = () => {
+        dispatch(fetchPastTableData(dateStart, timeStart, dateEnd, timeEnd))
+        dispatch(fetchPastPieData(dateStart, timeStart, dateEnd, timeEnd))
+        dispatch(fetchPastHistogramData(dateStart, timeStart, dateEnd, timeEnd))
+    }
+
     const columns = [
         {
             Header: '№',
             accessor: 'ratingRecordId',
-            width: 70 /scale
+            width: 70 / scale
         },
         {
             Header: 'Оператор',
@@ -69,7 +97,7 @@ const MonitoringCCPast = () => {
         {
             Header: 'Среднее время разговора',
             accessor: 'avgServiceTime',
-            width: 110/ scale
+            width: 110 / scale
         },
         {
             Header: 'Загруженность',
@@ -80,6 +108,8 @@ const MonitoringCCPast = () => {
 
     const domainYAxisCalls = findMaxAcceptAndNotAcceptSum(monitoringPastData)
 
+
+
     return (
         <div className={s.monitoringCCWrapper}>
             <Sidebar isActive={isActive}>
@@ -89,7 +119,36 @@ const MonitoringCCPast = () => {
                         <span>Параметры отбора</span>
                     </div>
                     <div className={s.optionContent}>
-                        <CustomTabs/>
+                        <Form.Group
+                            style={{display: "flex", justifyContent: "flex-end", flexDirection: "column"}}>
+                            <div>
+                                <Form.Label style={{color: "white", marginRight: "10px"}}>С:</Form.Label>
+                            </div>
+                            <div>
+                                <Form.Control type="date" defaultValue={dateStart} style={{width: "95%"}}
+                                              onChange={onDateStartChangeHandler}/>
+                                <Form.Control type="time" defaultValue={timeStart} style={{width: "95%"}}
+                                              onChange={onTimeStartChangeHandler}/>
+                            </div>
+                        </Form.Group>
+                        <Form.Group
+                            style={{
+                                display: "flex",
+                                justifyContent: "flex-end",
+                                flexDirection: "column",
+                                marginBottom: "15px"
+                            }}>
+                            <div>
+                                <Form.Label style={{color: "white", marginRight: "10px"}}>По:</Form.Label>
+                            </div>
+                            <div>
+                                <Form.Control type="date" defaultValue={dateEnd} style={{width: "95%"}}
+                                              onChange={onDateEndChangeHandler}/>
+                                <Form.Control type="time" defaultValue={timeEnd} style={{width: "95%"}}
+                                              onChange={onTimeEndChangeHandler}/>
+                            </div>
+                        </Form.Group>
+                        <TabButton style={{marginTop: "0px"}} name={'Обновить'} onClick={onLoadDataHandler}/>
                     </div>
                 </div>
             </Sidebar>
@@ -106,7 +165,8 @@ const MonitoringCCPast = () => {
                     </div>
                     <div className={s.ratingContainer}>
                         <span>Рейтинг операторов</span>
-                        <div className={s.table}><Table data={operatorsRatingData} columns={columns} height={"40vh"}/></div>
+                        <div className={s.table}><Table data={operatorsRatingData} columns={columns} height={"40vh"}/>
+                        </div>
                     </div>
                 </div>
                 <div className={s.histogram}>
