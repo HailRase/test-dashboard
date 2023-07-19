9858c8c1dbcbdf8db549c0857ddc01eb71e13621
HailRase <yurii_klimko@mail.ru>
2023-07-19 10:43:51 +0300

    set changes

--- src/s1-main/m3-dal/d1-instance/instance.ts
+++ src/s1-main/m3-dal/d1-instance/instance.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const instance = axios.create({
      /*baseURL: 'http://192.168.1.60:50300/',*/
-     baseURL: 'http://192.168.1.124:50300/',
+     baseURL: 'http://192.168.1.124:50300/', //your local ip address
+
      /*baseURL: 'http://192.168.43.218:50300/',*/
     /*baseURL: 'http://10.5.105.80:50300/',*/
     /*baseURL: 'http://127.0.0.1:8089/'*/
